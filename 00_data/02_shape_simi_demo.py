b4566adc76322f95d294b0daa258d4b6506d14ab
liuyao <liuyao@zhito.com>
2023-07-05 16:52:32 +0800

    add shape similarity demo
    Change-Id: Ie985b706cce241cdcf2f1c56f75f188127a7306c

--- 00_data/02_shape_simi_demo.py
+++ 00_data/02_shape_simi_demo.py
@@ -3,11 +3,14 @@
 
 import akshare as ak
 import numpy as np
-import pandas as pd
-from zigzag import *
+from functools import reduce
 
 import matplotlib.pyplot as plt
-
+import pandas as pd
+from shapesimilarity import shape_similarity
+import similaritymeasures
+import math
+# import zigzag 
 
 # 下载A股所有数据
 # all_df = ak.stock_zh_a_spot_em()
@@ -23,8 +26,8 @@ t_df_daily = ak.stock_zh_a_hist(symbol="600640", period = "daily", start_date= "
 
 X = df_daily["收盘"]
 x_t = t_df_daily["收盘"]
-pivots = peak_valley_pivots(X, 0.03, -0.03)
-t_pivots = peak_valley_pivots(x_t, 0.03, -0.03)
+# pivots = zigzag.peak_valley_pivots(X, 0.03, -0.03)
+# t_pivots = zigzag.peak_valley_pivots(x_t, 0.03, -0.03)
 
 # 添加数值、百分比
 def plot_pivots(X, pivots):
@@ -36,8 +39,18 @@ def plot_pivots(X, pivots):
     plt.scatter(np.arange(len(X))[pivots == -1], X[pivots == -1], color='r')
       
 
-plot_pivots(X, t_pivots)
-plt.show()
+# plot_pivots(X, pivots)
+# plt.show()
+
+x = np.linspace(1, -1, num=83)
+
+shape1 = np.column_stack((x, X))
+shape2 = np.column_stack((x, x_t))
+
+similarity = shape_similarity(shape1, shape2)
+# Discrete Frechet distance
+print(f"shape_similarity similarity {similarity}")
+
 
 
 
