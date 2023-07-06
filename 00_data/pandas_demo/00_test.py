e6b2c06eaa5e8ec68d6f5328ffc71b25b594db35
liuyao <liuyao@zhito.com>
2023-07-06 19:33:34 +0800

    add zigzag function
    Change-Id: I1b178aee88dda877e346851bedb843044470ed8a

--- 00_data/pandas_demo/00_test.py
+++ 00_data/pandas_demo/00_test.py
@@ -7,6 +7,14 @@ df = pd.DataFrame({'one': pd.Series(np.random.randn(3), index=['a', 'b', 'c']),
                    'two': pd.Series(np.random.randn(4), index=['a', 'b', 'c', 'd']),
                    'three': pd.Series(np.random.randn(3), index=['b', 'c', 'd'])})
 
-print(df)
+# print(df)
 
-print(df['two'])
+# print(df.apply(lambda x: x.max() - x.min()))
+
+# print(df.apply(np.exp))
+tsdf = pd.DataFrame(np.random.randn(10, 3), columns=['A', 'B', 'C'],
+                 index=pd.date_range('1/1/2000', periods=10))
+
+tsdf.iloc[3:7] = np.nan
+
+print(tsdf["A"].agg(np.sum))
