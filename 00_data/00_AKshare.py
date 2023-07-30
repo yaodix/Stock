6b11592821b309f6b6e9965ec9f221fba24605e0
yaodix <liuyao199111@163.com>
2023-07-31 01:27:26 +0800

    diweiqidong

--- 00_data/00_AKshare.py
+++ 00_data/00_AKshare.py
@@ -13,29 +13,6 @@ from  tqdm import tqdm
 import matplotlib.pyplot as plt
 
 
-# 下载A股所有数据
-# all_df = ak.stock_zh_a_spot_em()
-
-#  stock_sh_a_spot_em  # 沪A
-#  stock_sz_a_spot_em  # 深A
- 
-#  stock_individual_info_em    # 个股信息查询
- 
-# print(all_df.head())
-
-# 下载个股日k数据图
-# 20230131-20230531,　形态, 83周期
-# target: 600640，20230303 - 20030705
-
-def test_one():
-  df_daily = ak.stock_zh_a_hist(symbol="000338", period = "daily", start_date= "20230131", end_date="20230531")
-  t_df_daily = ak.stock_zh_a_hist(symbol="600640", period = "daily", start_date= "20230302", end_date="20230704")
-  print(df_daily.tail())
-
-  X = df_daily["收盘"]
-  print(X.tail())
-
-
 def get_sh_sz_A_name():
 # 获取所有深A,沪A股市代码,过滤ST，新股、次新股
 # 
@@ -44,7 +21,6 @@ def get_sh_sz_A_name():
   #获取全部股票代码
   stock_info_a_code_name_df = ak.stock_info_a_code_name()
   total_codes = stock_info_a_code_name_df['code'].tolist()
-
 
           
   #非科创板、非创业板、非北京
@@ -91,31 +67,6 @@ def get_sh_sz_A_name():
   return df     
         
 
-def test_merge():
-  df1 = pd.DataFrame(
-    {
-        "A": ["A0", "A1", "A2", "A3"],
-        "B": ["B0", "B1", "B2", "B3"],
-        "C": ["C0", "C1", "C2", "C3"],
-        "D": ["D0", "D1", "D2", "D3"],
-    },
-    index=[0, 1, 2, 3],)
-
-
-  df2 = pd.DataFrame(
-      {
-          "A": ["A4", "A5", "A6", "A7"],
-          "B": ["B4", "B5", "B6", "B7"],
-          "C": ["C4", "C5", "C6", "C7"],
-          "D": ["D4", "D5", "D6", "D7"],
-      },
-    index=[0, 1, 2, 3],)
-
-  frames = [df1, df2]
-
-  result = pd.concat(frames, ignore_index=True)
-  print(f'merge {result}')
-  
 
 def get_security_info(symbol = str):
     # 
@@ -205,10 +156,89 @@ def yaogu(decrease_years = 2.5, increse_day = 12):
       # if (output_cnt %100 == 0):
         # print(f"out code")
 
+def diweiqidong(ratio = 0.7, all_days = 250*5, long_time_days = 250*3, short_time_days= 20*4):
+  '''
+  启动信号
+    1. 2次涨停(或6%以上大阳线)，但股价没有大幅涨起来，青山纸业，间隔3个月60天交易日，有2次涨停
+    2. 低位波动涨跌一次，到低位
+
+  test: 青山纸业 600103   20230302-20230529
+  test: 学大教育 000526   20230130-20230428
+  '''
+  stocks = get_sh_sz_A_name()
+  
+  for code in tqdm(stocks.code.tolist()):
+    # code = "600103"
+    end_day = dt.date(dt.date.today().year,dt.date.today().month,dt.date.today().day)
+    
+    #考虑到周六日非交易
+    days = long_time_days * 7 / 5
+    start_day = end_day - dt.timedelta(days)
+
+    start_day = start_day.strftime("%Y%m%d")
+    end_day = end_day.strftime("%Y%m%d")   
+    # end_day = "20230529"
+    
+    df_daily = ak.stock_zh_a_hist(symbol=code, period = "daily", start_date=start_day, end_date= end_day, adjust= 'qfq')
+    
+    # max loc
+    # print(f'df_daily {df_daily.tail()}')
+    close = df_daily.收盘
+    increase = df_daily.涨跌幅
+    # print(f'close {close.head()}')
+    
+    max_price_index = close[:-short_time_days*2].idxmax()
+    max_price = close[:-short_time_days*2].max()
+    
+    min_price_near_index = close[-short_time_days*2:].idxmin()
+    min_price_near = close[-short_time_days*2:].min()
+    # 低价最近发生
+    if min_price_near < 1:  # 即将退市股票
+      continue
+    
+    # 双响炮
+    # 最近15天内有涨幅
+    break_2_index = increase[-15:-5].idxmax()
+    break_2_ratio = increase[-15:-5].max()
+    inc_2_val = close[break_2_index]
+
+    break_1_f = increase[-short_time_days:break_2_index]
+    break_1_index = increase[-short_time_days:break_2_index].idxmax()
+    break_1_ratio = increase[-short_time_days:break_2_index:].max()
+    inc_1_val = close[break_1_index]
+
+
+  # 最近10天涨幅超过6，只有2个
+    cnt = np.sum(increase[-10:]>=6)  
+    # 涨停后回调
+    cnt2 = np.sum(close[-5:] < inc_2_val) 
+
+    # 两个
+  
+  # 最近价格都小于
+
+    if break_1_ratio > 8 and break_2_ratio > 8 and abs(inc_2_val - inc_1_val)/ inc_1_val < 0.2 and abs(inc_2_val - inc_1_val)/ inc_2_val < 0.2 and break_2_index - break_1_index > 20 and \
+       min_price_near_index > max_price_index and min_price_near / max_price < ratio and  min(inc_2_val, min_price_near) / max(inc_2_val ,min_price_near) > 0.65 and \
+        cnt <=2  and cnt2 > 3:
+      print(code)
+      print(f"break 1 {break_1_ratio}, data {df_daily.loc[break_1_index]['日期']}")
+      print(f"break 2 {break_2_ratio}, data {df_daily.loc[break_2_index]['日期']}")
+
+
+      # break
+    # 与之前10年股价差排序
+
+    # 低位横盘长度排序
+    # break
+    
+  return
+
+
 
 if __name__ == "__main__":
     # n = get_sh_sz_A_name()
     # print(n.head)
     # res = is_break_low()
-    yaogu()
+    # yaogu()
+    diweiqidong()
 
