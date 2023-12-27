f72fe6a2d4f6189bf5427baaabb4e52ef5dbf68d
liuyao <liuyao@zhito.com>
2023-12-27 17:47:50 +0800

    do slow bull
    Change-Id: I94c67e49d134572b217875cd7337208ce952825b

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ from  tqdm import tqdm
 import matplotlib.pyplot as plt
 
 
-stock_individual_info_em_df = ak.stock_individual_info_em(symbol="000001")
+a = np.array([1,3,6,8])
+b = np.array([1,2,6,8])
 
-print(stock_individual_info_em_df["value"][0])
+print(np.sum(a > b))
